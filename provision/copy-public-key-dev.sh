72f61d5cee3ce815a53e27c6d02706a060fdc988
akafred <github@akafred.com>
2017-04-13 16:53:50 +0200

    Fix public-key copy-issue

--- provision/copy-public-key-dev.sh
+++ provision/copy-public-key-dev.sh
@@ -1,2 +1,2 @@
-vagrant up
+vagrant up devmain
 cat ssh/id_rsa.pub | vagrant ssh devmain -c 'cat >> .ssh/authorized_keys && echo "Key copied"'
--- provision/copy-public-key-stage.sh
+++ provision/copy-public-key-stage.sh
@@ -1,2 +1,2 @@
-vagrant up
+vagrant up stagemain
 cat ssh/id_rsa.pub | vagrant ssh stagemain -c 'cat >> .ssh/authorized_keys && echo "Key copied"'
